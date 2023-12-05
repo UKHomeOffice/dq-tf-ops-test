191141d8022f00df48999bf462d67b1448029ea6
Alastair Allan <alastair.allan@digital.homeoffice.gov.uk>
2023-12-05 08:46:50 +0000

    Fix account id references

--- analysis.tf
+++ analysis.tf
@@ -354,7 +354,7 @@ EOF
 
 resource "aws_iam_role_policy_attachment" "dq_tf_infra_write_to_cw_analysis" {
   role       = aws_iam_role.httpd_ec2_server_role.id
-  policy_arn = "arn:aws:iam::${var.account_id[var.namespace]}:policy/dq-tf-infra-write-to-cw"
+  policy_arn = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:policy/dq-tf-infra-write-to-cw"
 }
 
 resource "aws_iam_role_policy_attachment" "httpd_ec2_server_CWagent" {
