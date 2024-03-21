bb65d04da23ab14dcc7b84a5d93a2f1a8666fd6c
blake-mck-HO <DEV\xd-mckenzb@a-2dwah8l6xsuds.dev.dacc.phz>
2024-03-21 17:29:35 +0000

    added formatting changes

--- analysis.tf
+++ analysis.tf
@@ -206,14 +206,13 @@ resource "aws_s3_bucket_acl" "httpd_config_bucket_acl" {
 
 resource "aws_s3_bucket_versioning" "httpd_config_bucket_versioning" {
   bucket = "${var.httpd_config_bucket_name}-${var.namespace}"
-    versioning_configuration {
-      status = "Enabled"
+  versioning_configuration {
+    status = "Enabled"
   }
 }
 
 resource "aws_s3_bucket_server_side_encryption_configuration" "httpd_config_bucket_server_side_encryption_configuration" {
   bucket = "${var.httpd_config_bucket_name}-${var.namespace}"
-
   rule {
       apply_server_side_encryption_by_default {
       kms_master_key_id = aws_kms_key.httpd_config_bucket_key.arn
