3cc164c61fdce6693a8bb29c963f098bc12f593c
blake-mck-HO <DEV\xd-mckenzb@a-2dwah8l6xsuds.dev.dacc.phz>
2024-03-21 17:34:46 +0000

    added formatting changes

--- analysis.tf
+++ analysis.tf
@@ -215,8 +215,8 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "httpd_config_buck
   bucket = "${var.httpd_config_bucket_name}-${var.namespace}"
   rule {
     apply_server_side_encryption_by_default {
-    kms_master_key_id = aws_kms_key.httpd_config_bucket_key.arn
-    sse_algorithm     = "aws:kms"
+      kms_master_key_id = aws_kms_key.httpd_config_bucket_key.arn
+      sse_algorithm     = "aws:kms"
     }
   }
 }
